13f8c0c5ff9f06f6c5e8a11c98f1f475ca70b50f
sarelpirates <48471504+sarelpirates@users.noreply.github.com>
2023-05-23 17:19:33 +0800

    updated appointment allocation

--- UtmCounselingSystem/Data/AppointmentAllocation.cs
+++ UtmCounselingSystem/Data/AppointmentAllocation.cs
@@ -4,13 +4,11 @@ namespace UtmCounselingSystem.Data
 {
     public class AppointmentAllocation : BaseEntity
     {
-        public int Id { get; set; }
         public int NumberOfDays { get; set; }
         [ForeignKey("AppointmentTypeId")]
         public AppointmentType AppointmentType { get; set; }
         public int AppointmentTypeId { get; set; }
         public string ClientId { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
+        public int Period { get; set; }
     }
 }
