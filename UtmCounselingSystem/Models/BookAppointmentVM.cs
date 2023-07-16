61b9ccf55f5dc0b3a84c65fd3bf2c23821f3a741
sarelpirates <48471504+sarelpirates@users.noreply.github.com>
2023-07-16 09:29:15 +0800

    ok

---
+++ UtmCounselingSystem/Models/BookAppointmentVM.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace UtmCounselingSystem.Models
+{
+    public class BookAppointmentVM
+    {
+        [Display(Name = "UTM ID ")]
+        [Required]
+        public int Id { get; set; }
+
+        [Display(Name = "Name ")]
+        [Required]
+        public string? Name { get; set; }
+
+        [Display(Name = "Email")]
+        [Required]
+        public string? Email { get; set; }
+
+
+        [Display(Name = "Phone Number")]
+        [Required]
+        public int PhoneNumber { get; set; }
+
+        [Display(Name = "Remark")]
+        [Required]
+        public string? Remark { get; set; }
+    }
+}
